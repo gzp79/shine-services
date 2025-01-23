e696f95eacb068d814dbc4451f42b0ae80d04f7e
gzp79 <->
2025-01-23 14:26:45 +0100

    some log mods

--- docker/scripts/start-builder.sh
+++ docker/scripts/start-builder.sh
@@ -4,6 +4,8 @@ export AZURE_TENANT_ID="${IDENTITY_TENANT_ID}"
 export AZURE_CLIENT_ID="${IDENTITY_CLIENT_ID}"
 export AZURE_CLIENT_SECRET="${IDENTITY_CLIENT_SECRET}"
 
+export RUST_LOG="INFO,shine_core=TRACE,shine_identity=TRACE,shine_builder=TRACE"
+
 echo "Starting service for stage: ${ENVIRONMENT}..."
 cd ./services/builder
 ./shine-builder ${ENVIRONMENT}
